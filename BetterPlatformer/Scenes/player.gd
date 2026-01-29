b952923ae19e085a908788dce52498f04d3246d1
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-01-29 11:47:56 +0700

    Fixed Coyote Time and Variable Jump Height

--- BetterPlatformer/Scenes/player.gd
+++ BetterPlatformer/Scenes/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var speed: float = 150
 @export var jump_velocity: float = -270
 @export var terminal_velocity: float = 500
-@export var fall_speed: float = 980
+@export var default_gravity: float = 980
 
 @export_category("Buffer Jump")
 @export var jump_buffer_time: float = 0.1
@@ -13,21 +13,25 @@ extends CharacterBody2D
 @export var coyote_time: float = 0.1
 
 @export_category("Variable Jump Height")
-@export var gravity_modifier: float = 8
-
+@export var gravity_modifier: float = 2
 
 @onready var debug_label: Label = $DebugLabel
 @onready var buffer_timer: Timer = $BufferTimer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var sprite: AnimatedSprite2D = $Sprite
 
-# Buffer Jump
+# Buffer Jump & Coyote Time
 var jump_available: bool = true
-var buffer_jump: bool = false
+var jump_buffer: bool = false
+
+# Current Gravity
+var current_gravity: float = 0
 
 func _ready() -> void:
 	buffer_timer.wait_time = jump_buffer_time
 	coyote_timer.wait_time = coyote_time
+	current_gravity = default_gravity
+	Engine.time_scale = 0.2
 
 func _physics_process(delta: float) -> void:
 	update_debug_label()
@@ -42,26 +46,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func handle_fall(delta: float) -> void:
-	var current_fall_speed: float
-	if Input.is_action_just_released("jump") and !is_on_floor():
-		current_fall_speed = fall_speed * gravity_modifier
+	if !is_on_floor():
+		if jump_available and coyote_timer.is_stopped():
+			coyote_timer.start()
+		
+		if velocity.y < 0 and Input.is_action_just_released("jump"): # If Falling
+			current_gravity = default_gravity * gravity_modifier
+			
+		velocity.y += current_gravity * delta
 	else:
-		current_fall_speed = fall_speed
-	velocity.y += delta * current_fall_speed
+		jump_available = true
+		current_gravity = default_gravity
+		coyote_timer.stop()
 
 func handle_jump() -> void:
-	if Input.is_action_just_pressed("jump"):
-		if jump_available:
-			jump()
-		else: #If jump unavailable
-			buffer_jump = true
-			buffer_timer.start()
-	
-	if is_on_floor():
-		if buffer_jump:
-			jump()
-			buffer_timer.stop()
-			buffer_jump = false
+	if Input.is_action_just_pressed("jump") and jump_available:
+		jump()
 
 func handle_movement() -> void:
 	var direction := Input.get_axis("left", "right")
@@ -84,15 +84,14 @@ func jump() -> void:
 func update_debug_label() -> void:
 	var text: String = ""
 	text += "BUFF: %.2f\n" % buffer_timer.time_left
-	text += "VEL: %v" % velocity
+	text += "COY: %.2f\n" % coyote_timer.time_left
+	text += "GRAV: %.2f," % current_gravity
+	text += "AVA: %s" % jump_available
 	debug_label.text = text
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		get_tree().paused = !get_tree().paused
 
-func _on_buffer_timer_timeout() -> void:
-	buffer_jump = false
-
 func _on_coyote_timer_timeout() -> void:
-	pass # Replace with function body.
+	jump_available = false
